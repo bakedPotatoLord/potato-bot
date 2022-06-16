a4f146cc3521e7b92ba65c3b38c679f4134f14df
BakedPotatoLord <918780@dpsk12.net>
2022-06-16 17:27:32 -0600

    fixed syntax error

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const {client} = reqire("../index.js")
+const {client} = require("../index.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
