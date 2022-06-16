98e885bbcfb5c878caa06ce068f4f2a47830ee0a
BakedPotatoLord <918780@dpsk12.net>
2022-06-16 16:16:36 -0600

    created echo command

---
+++ commands/echo.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs/promises")
+
+module.exports={
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription("echoes user input")
+        .addStringOption(option => 
+            option.setName('input')
+            .setDescription('The input to echo back')
+            .setRequired(true)),
+
+        async execute(interaction){
+
+            let data = await fs.readFile("users.json","utf-8")
+
+            return interaction.reply(`you said ${interaction.options.getString("input")} `)
+        }
+}
+
