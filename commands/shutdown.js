c1d8d53faea9ae29c67500f15f7c702d786c5c54
BakedPotatoLord <918780@dpsk12.net>
2022-06-16 17:34:23 -0600

    finalized code

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,13 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const {client} = require("../index.js")
+//const {client} = require("../index.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shutdown')
 		.setDescription('shut down the server'),
+
 	async execute(interaction) {
         if(interaction.user.id == "750347468826345492"){
-            client.destroy()
+
             process.exit()
         }else{
             return interaction.reply(`no way you loser`);
