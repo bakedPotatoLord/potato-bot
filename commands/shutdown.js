302928691f3b6408d80763542ca4efe609ff03d4
BakedPotatoLord <918780@dpsk12.net>
2022-06-16 17:25:08 -0600

    added shutdown code

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -8,7 +8,10 @@ module.exports = {
 	async execute(interaction) {
         if(interaction.user.id == "750347468826345492"){
             client.destroy()
+            process.exit()
+        }else{
+            return interaction.reply(`no way you loser`);
         }
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		
 	},
 };
