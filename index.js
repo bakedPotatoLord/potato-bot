4f6f5afc8f27673393ae843dac77b41f16b4868d
BakedPotatoLord <918780@dpsk12.net>
2022-06-17 15:04:49 -0600

    made dissme command less specific

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ client.on('interactionCreate', async interaction => {
 
 client.on('messageCreate', message => {
 	console.log("recived a message")
-
-	if(message.author.id == "790765656571379762" || message.content.toLowerCase() == "diss me"){
+	
+	if(message.author.id == "790765656571379762" || checkforWord(message.content,"diss me")){
 
 		message.react('😡')
 		message.reply(disses[Math.floor(Math.random()*disses.length)])
