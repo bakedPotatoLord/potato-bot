c901b5bd80b195cde9197d045142f9f19faa3ff0
BakedPotatoLord <918780@dpsk12.net>
2022-06-17 15:09:25 -0600

    created diss command

---
+++ commands/dissMe.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const disses = require("../disses.json")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dissMe')
+		.setDescription('returns a diss'),
+	async execute(interaction) {
+		return interaction.reply(disses[Math.floor(Math.random()*disses.length)])
+	},
+};
