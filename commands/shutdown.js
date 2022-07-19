a2c7d6bf6705a3c6acf6d480b046260ed9e7e354
= <918780@dpsk12.net>
2022-07-19 11:44:59 -0600

    changed bot description

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shutdown')
-		.setDescription('shut down the server'),
+		.setDescription('shut down the bot'),
 
 	async execute(interaction) {
         if(interaction.user.id == "750347468826345492" || interaction.user.id == "530508910713372682"){
