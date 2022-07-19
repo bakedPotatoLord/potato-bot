53c19a1b07601fe63da4a85b886e66cdb94729ee
= <918780@dpsk12.net>
2022-07-19 12:26:00 -0600

    added main functionality of fight command

--- commands/fight.js
+++ commands/fight.js
@@ -16,10 +16,40 @@ module.exports={
 
             let data = JSONToMap( await fs.readFile("users.json","utf-8") )
 
+            let fightString = 'fight started\n'
+
             if(data.has(interaction.options.getUser("user").id) && data.has(interaction.user.id)){
-                await interaction.reply('fight started')
-                await new Promise(res=>{setTimeout(res,2000)})
-                await interaction.editReply("fight ended")
+                await interaction.reply(fightString)
+                await new Promise(res=>{setTimeout(res,1000)})
+
+                let p1Data = data.get(interaction.user.id)
+                let p2Data = data.get(interaction.options.getUser("user").id)
+
+                if(p1Data.atk > p2Data.def){
+                    fightString+=`<@${p1Data.user.id}> attacks, causing 1 damage\n`
+                    //p2Data.health --
+                }else{
+                    fightString+=`<@${p1Data.user.id}> attacks, causing 0 damage\n`
+                }
+                await interaction.editReply(fightString)
+
+                //wait for a second
+                await new Promise(res=>{setTimeout(res,1000)})
+
+                if(p2Data.atk > p1Data.def){
+                    fightString+=`<@${p2Data.user.id}> attacks, causing 1 damage\n`
+                    //p1Data.health --
+                }else{
+                    fightString+=`<@${p2Data.user.id}> attacks, causing 0 damage\n`
+                }
+                await interaction.editReply(fightString)
+
+                await new Promise(res=>{setTimeout(res,1000)})
+
+                fightString+="fight ended"
+                await interaction.editReply(fightString)
+                
+                await fs.writeFile("./users.json",mapToJSON(data))
                 return
             }else{
                 return interaction.reply('both users must be registered')
