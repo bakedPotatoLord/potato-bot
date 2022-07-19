9b7110ec7bc80163ed7102728e944662d8e16d77
= <918780@dpsk12.net>
2022-07-19 10:16:02 -0600

    added contingency for if a person is not registerd

--- commands/stats.js
+++ commands/stats.js
@@ -10,20 +10,24 @@ module.exports={
             option.setName('user')
             .setDescription('The user to check')
             .setRequired(true)
-
         ),
 
         async execute(interaction){
 
             let data = JSONToMap( await fs.readFile("users.json","utf-8") )
-            let userData = data.get(interaction.options.getUser("user").id)
 
-            return interaction.reply(`
-            ${interaction.options.getUser("user")}'s stats: \n
-            Balance: ${userData.bal}\n
-            Health: ${userData.health}\n
-            Atk: ${userData.atk}\n
-            Def: ${userData.def}
-            `)
+            if(data.has(interaction.options.getUser("user").id)){
+                let userData = data.get(interaction.options.getUser("user").id)
+
+                return interaction.reply(`
+                ${interaction.options.getUser("user")}'s stats: \n
+                Balance: ${userData.bal}\n
+                Health: ${userData.health}\n
+                Atk: ${userData.atk}\n
+                Def: ${userData.def}
+                `)
+            }else{
+                return interaction.reply('i got no idea who this is. have them register')
+            }
         }
 }
