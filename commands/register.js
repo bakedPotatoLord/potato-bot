7068d8304ba1441ed1f239481eb4d72fa5f342c9
= <918780@dpsk12.net>
2022-07-19 12:06:23 -0600

    added more user info

--- commands/register.js
+++ commands/register.js
@@ -10,17 +10,13 @@ module.exports={
         async execute(interaction){
 
             let users = JSONToMap( await fs.readFile("./users.json","utf-8") )
-            console.log(users)
-
 
             if(users.has(interaction.user.id)){
                 return interaction.reply("you are already registered")
             }else{
-                users.set(interaction.user.id,{"name":interaction.user.name,"balance":0,"atk":0,"def":0,"health":10})
+                users.set(interaction.user.id,{"user":interaction.user,"balance":0,"atk":0,"def":0,"health":10})
                 await fs.writeFile("./users.json",mapToJSON(users))
                 return interaction.reply(`user added`)
             }
-
-            
         }
 }
