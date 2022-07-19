4cb0d974fd904c94813210e9f88d27cd0844dcc8
= <918780@dpsk12.net>
2022-07-19 16:18:08 -0600

    created regen command

---
+++ commands/regen.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs/promises");
+const { JSONToMap, mapToJSON } = require('../helpers/mapToJson');
+
+module.exports={
+    data: new SlashCommandBuilder()
+        .setName('regen')
+        .setDescription("regenerates health"),
+
+
+        async execute(interaction){
+
+            let data = JSONToMap( await fs.readFile("users.json","utf-8") )
+
+            if(data.has(interaction.user.id)){
+
+                let userData = data.get(interaction.user.id)
+                if(userData.health >= 10){
+                    return await interaction.reply('health is already maxed')
+                }else if( Date.now() - userData.lastRegen  <= 60000){
+                    return await interaction.reply(`you must wait ${60-Math.round(Date.now() - userData.lastRegen)} more secs to regenerate health`)
+                }else{
+                    userData.health++
+                    userData.lastRegen = Date.now()
+
+                    await interaction.reply('regenerating')
+                    for(let i = 0 ;i<10;i++){
+                        let healthString ='regenerating\n'
+                        for(j=0;j<10;j++){
+                            if(j>i){
+                                healthString +='⬜'
+                            }else{
+                                healthString +='🟩'
+                            }
+                        }
+                        await interaction.editReply(healthString)
+                        await new Promise(res=>setTimeout(res,500))
+                    }
+                    await interaction.editReply(`health regenerated. health is now ${userData.health}`)
+                }
+                
+        
+                data.set(interaction.user.id,userData)
+                await fs.writeFile('users.json',mapToJSON(data))
+                return
+            }else{
+                return interaction.reply('register to use /regen')
+            }
+        }
+}
