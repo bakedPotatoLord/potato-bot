4588bd294936dfcacb93a444b095df8e19ff5992
= <918780@dpsk12.net>
2022-07-20 09:08:35 -0600

    created user manipulation helper

---
+++ helpers/editUsers.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises")
+const {mapToJSON,JSONToMap} = require('../helpers/mapToJson')
+
+async function editUsers(){
+
+    let data = JSONToMap( await fs.readFile("users.json","utf-8") )
+
+    for(i of data){
+        console.log(i)
+        let userData = data.get(i[0])
+        //put user manipulations here
+        
+        userData.energy = 10
+
+        data.set(i[0],userData)
+    }
+
+    await fs.writeFile('users.json',mapToJSON(data))
+}
+
+editUsers()
+
