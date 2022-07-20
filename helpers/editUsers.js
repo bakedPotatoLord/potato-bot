6959ba7395b3deeae0625567b0af18ff16578cdd
= <918780@dpsk12.net>
2022-07-20 09:10:39 -0600

    removed console logs

--- helpers/editUsers.js
+++ helpers/editUsers.js
@@ -2,19 +2,16 @@ const fs = require("fs/promises")
 const {mapToJSON,JSONToMap} = require('../helpers/mapToJson')
 
 async function editUsers(){
-
     let data = JSONToMap( await fs.readFile("users.json","utf-8") )
 
     for(i of data){
-        console.log(i)
         let userData = data.get(i[0])
         //put user manipulations here
-        
+
         userData.energy = 10
 
         data.set(i[0],userData)
     }
-
     await fs.writeFile('users.json',mapToJSON(data))
 }
 
