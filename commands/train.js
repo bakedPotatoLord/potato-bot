487266bc0cae71058c35df2f23f424d38f90345c
= <918780@dpsk12.net>
2022-07-20 12:12:08 -0600

    added chad joke

--- commands/train.js
+++ commands/train.js
@@ -22,15 +22,20 @@ module.exports={
 
             let userData = data.get(interaction.user.id)
 
-            if(interaction.options.getString("stat")=="atk"){
-                userData.atk +=1
-                await interaction.reply(`trained atk. atk is now ${userData.atk}`)
-            }else if(interaction.options.getString("stat")=="def"){
-                userData.def+=1
-                await interaction.reply(`trained def. def is now ${userData.def}`)
-            }
-            
 
+            if( Date.now() - userData.lastTrain  <= 60000){
+                return await interaction.reply(`Slow down there Chad. you must wait ${Math.round(60-(Date.now() - userData.lastTrain)/1000)} more secs to train again`)
+            }else{
+                userData.lastTrain = Date.now()
+
+                if(interaction.options.getString("stat")=="atk"){
+                    userData.atk +=1
+                    await interaction.reply(`trained atk. atk is now ${userData.atk}`)
+                }else if(interaction.options.getString("stat")=="def"){
+                    userData.def+=1
+                    await interaction.reply(`trained def. def is now ${userData.def}`)
+                }
+            }
 
             data.set(interaction.user.id, userData )
             await fs.writeFile("users.json",mapToJSON(data))
