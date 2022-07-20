61add4af31073b9e372effcd8ec64ced46fba9b8
= <918780@dpsk12.net>
2022-07-20 09:21:44 -0600

    disalowed users from fighiting themself

--- commands/fight.js
+++ commands/fight.js
@@ -19,38 +19,45 @@ module.exports={
             let fightString = 'fight started\n'
 
             if(data.has(interaction.options.getUser("user").id) && data.has(interaction.user.id)){
-                await interaction.reply(fightString)
-                await new Promise(res=>{setTimeout(res,1000)})
 
-                let p1Data = data.get(interaction.user.id)
-                let p2Data = data.get(interaction.options.getUser("user").id)
-
-                if(p1Data.atk > p2Data.def){
-                    fightString+=`<@${p1Data.user.id}> attacks, causing 1 damage\n`
-                    //p2Data.health --
+                if(interaction.options.getUser("user").id == interaction.user.id ){
+                    return interaction.reply('you cant fight yourself ')
                 }else{
-                    fightString+=`<@${p1Data.user.id}> attacks, causing 0 damage\n`
+                    await interaction.reply(fightString)
+                    await new Promise(res=>{setTimeout(res,1000)})
+
+                    let p1Data = data.get(interaction.user.id)
+                    let p2Data = data.get(interaction.options.getUser("user").id)
+
+                    if(p1Data.atk > p2Data.def){
+                        fightString+=`<@${p1Data.user.id}> attacks, causing 1 damage\n`
+                        //p2Data.health --
+                    }else{
+                        fightString+=`<@${p1Data.user.id}> attacks, causing 0 damage\n`
+                    }
+                    await interaction.editReply(fightString)
+
+                    //wait for a second
+                    await new Promise(res=>{setTimeout(res,1000)})
+
+                    if(p2Data.atk > p1Data.def){
+                        fightString+=`<@${p2Data.user.id}> attacks, causing 1 damage\n`
+                        //p1Data.health --
+                    }else{
+                        fightString+=`<@${p2Data.user.id}> attacks, causing 0 damage\n`
+                    }
+                    await interaction.editReply(fightString)
+
+                    await new Promise(res=>{setTimeout(res,1000)})
+
+                    fightString+="fight ended"
+                    await interaction.editReply(fightString)
+                    
+                    await fs.writeFile("./users.json",mapToJSON(data))
+                    return
                 }
-                await interaction.editReply(fightString)
 
-                //wait for a second
-                await new Promise(res=>{setTimeout(res,1000)})
-
-                if(p2Data.atk > p1Data.def){
-                    fightString+=`<@${p2Data.user.id}> attacks, causing 1 damage\n`
-                    //p1Data.health --
-                }else{
-                    fightString+=`<@${p2Data.user.id}> attacks, causing 0 damage\n`
-                }
-                await interaction.editReply(fightString)
-
-                await new Promise(res=>{setTimeout(res,1000)})
-
-                fightString+="fight ended"
-                await interaction.editReply(fightString)
                 
-                await fs.writeFile("./users.json",mapToJSON(data))
-                return
             }else{
                 return interaction.reply('both users must be registered')
             }
