70dab3439d11fb40d048f7dfb4e101bde8ac1c96
= <918780@dpsk12.net>
2022-07-21 12:05:58 -0600

    made potates a BIGINT

--- helpers/mapToJSON.js
+++ helpers/mapToJSON.js
@@ -1,4 +1,19 @@
 module.exports={
-    mapToJSON:(map)=>{return JSON.stringify(Object.fromEntries(map))},
-    JSONToMap:(json)=>{return new Map(Object.entries(JSON.parse(json)))}
+    mapToJSON:(map)=>{
+        for(i of map){
+            let userData = i[1]
+            userData.potatoes = userData.potatoes.toString()+'n'
+            map.set(i[0],userData)
+        }
+        return JSON.stringify(Object.fromEntries(map))
+    },
+    JSONToMap:(json)=>{
+        let map = new Map(Object.entries(JSON.parse(json)))
+        for(i of map){
+            let userData = i[1]
+            userData.potatoes = BigInt( userData.potatoes )
+            map.set(i[0],userData)
+        }
+        return map
+    }
 }
